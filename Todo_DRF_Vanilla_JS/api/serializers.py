22193a4f2c82986b272346b192a6bc3425f0ff31
Hemanth <hemanthhari2000@gmail.com>
2021-06-11 20:42:30 +0530

    Add Todo DRF Vanilla Js

---
+++ Todo_DRF_Vanilla_JS/api/serializers.py
@@ -0,0 +1,11 @@
+
+from django.db.models import fields
+from rest_framework import serializers
+
+from .models import Task
+
+
+class TaskSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
